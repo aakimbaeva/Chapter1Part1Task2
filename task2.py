c4caffd25cf2ee3f3272698c03311ed62a5bf7db
Azhara <mac@MacBook-Air-mac-AJara.local>
2020-02-16 22:48:48 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,15 @@
+def square_number(number):
+    a = number ** 2
+    return a
+
+
+
+
+
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+    print("YOUR CODE IS CORRECT!")
+
+test_square_number()
